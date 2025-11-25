e35ee01807a6537c521bc0f2bb0c19f3ae154780
TannyPhuket <68314010019@phuketvc.ac.th>
2025-11-25 19:13:32 +0700

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,21 +1,19 @@
-// js/firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import {
+  getDatabase, ref, onValue, get
+} from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBHCnAFJHBz95ugYztMkxBa5b6fwqCZqfo",
   authDomain: "temperature-cold-guard.firebaseapp.com",
   databaseURL: "https://temperature-cold-guard-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "temperature-cold-guard",
-  storageBucket: "temperature-cold-guard.firebasestorage.app",
-  messagingSenderId: "29693405672",
-  appId: "1:29693405672:web:9815de4ba98e7e4cf3dc5d",
-  measurementId: "G-XDHBRJ9S3W"
+  storageBucket: "temperature-cold-guard.appspot.com",
+  messagingSenderId: "xxxx",
+  appId: "xxxx"
 };
 
-// Init Firebase
-const app = initializeApp(firebaseConfig);
-
-// Export DB
+export const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
-export { ref, onValue };
+export { ref, onValue, get };
