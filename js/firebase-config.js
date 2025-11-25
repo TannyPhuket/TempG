9511ed6ecb46c02814179216dfa7633df27f99c8
TannyPhuket <68314010019@phuketvc.ac.th>
2025-11-25 19:52:52 +0700

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,9 +1,6 @@
-// firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
-import {
-  getDatabase, ref, onValue, get
-} from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js";
-
+<script src="https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js"></script>
+<script>
 const firebaseConfig = {
   apiKey: "AIzaSyBHCnAFJHBz95ugYztMkxBa5b6fwqCZqfo",
   authDomain: "temperature-cold-guard.firebaseapp.com",
@@ -13,7 +10,22 @@ const firebaseConfig = {
   messagingSenderId: "xxxx",
   appId: "xxxx"
 };
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
 
-export const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
-export { ref, onValue, get };
+async function login() {
+  const username = document.getElementById("username").value.trim();
+  const password = document.getElementById("password").value.trim();
+
+  const snapshot = await db.ref("users/" + username).get();
+  if (!snapshot.exists()) { alert("ไม่พบบัญชีผู้ใช้นี้"); return; }
+
+  const data = snapshot.val();
+  if(data.password !== password){ alert("รหัสผ่านไม่ถูกต้อง"); return; }
+
+  const role = data.Role;
+  if(role==="Seller") window.location="dashboard_seller.html";
+  if(role==="Driver") window.location="dashboard_driver.html";
+  if(role==="Buyer")  window.location="dashboard_buyer.html";
+}
+</script>
